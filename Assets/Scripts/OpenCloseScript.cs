77b25b1da0f4bdcfc9796a2301222232142c1710
Dickson Afful <fiifi.dev@gmail.com>
2024-03-14 12:02:37 -0400

    add animations

---
+++ Assets/Scripts/OpenCloseScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OpenScript : MonoBehaviour
+{
+    private Animator mAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mAnimator == null) return; 
+
+        if(Input.GetKeyDown(KeyCode.O)) {
+            mAnimator.SetTrigger("TrOpen");
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            mAnimator.SetTrigger("TrClose");
+        }
+    }
+}
