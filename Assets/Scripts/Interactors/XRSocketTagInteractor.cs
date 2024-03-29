70db02e63492227e263672e511860ffc6de87751
Dickson Afful <fiifi.dev@gmail.com>
2024-03-29 09:49:55 -0400

    add inside and outside events

--- Assets/Scripts/Interactors/XRSocketTagInteractor.cs
+++ Assets/Scripts/Interactors/XRSocketTagInteractor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,8 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class XRSocketTagInteractor : XRSocketInteractor
 {
+    public static event Action OnInside;
+    public static event Action OnOutside;
 
     public string TargetTag;
 
@@ -12,5 +15,29 @@ public class XRSocketTagInteractor : XRSocketInteractor
     public override bool CanHover(IXRHoverInteractable interactable)
     {
         return base.CanHover(interactable) && interactable.transform.tag == TargetTag;
+    }
+
+    protected override void OnEnable()
+    {
+        GetComponent<XRSocketInteractor>().selectEntered.AddListener(HandleSocketEnter);
+        GetComponent<XRSocketInteractor>().selectExited.AddListener(HandleSocketExit);
+        base.OnEnable();
+    }
+
+    protected override void OnDisable()
+    {
+        GetComponent<XRSocketInteractor>().selectEntered.RemoveListener(HandleSocketEnter);
+        GetComponent<XRSocketInteractor>().selectExited.RemoveListener(HandleSocketExit);
+        base.OnDisable();
+    }
+
+    private void HandleSocketExit(SelectExitEventArgs arg0)
+    {
+        OnOutside?.Invoke();
+    }
+
+    private void HandleSocketEnter(SelectEnterEventArgs arg0)
+    {
+        OnInside?.Invoke();
     }
 }
