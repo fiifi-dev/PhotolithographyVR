f739a79534995901d8de60c0539d0f1c19ed8840
Dickson Afful <fiifi.dev@gmail.com>
2024-03-30 12:44:47 -0400

    temp refactor

--- Assets/Scripts/StageOne/StepTwo.cs
+++ Assets/Scripts/StageOne/StepTwo.cs
@@ -7,9 +7,11 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class StepTwo : PerformStep
 {
-    private bool CanPerformStep()
+
+
+    public override bool CanPerformStep()
     {
         var stage = BuildStage.GetCurrentStage();
-        return !IsDone && stage.ActiveStepId == 2 && !IsTimedCorrectly;
+        return !IsDone && stage.ActiveStepId == 2;
     }
 }
