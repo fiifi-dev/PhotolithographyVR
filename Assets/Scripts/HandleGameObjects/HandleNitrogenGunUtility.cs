59786320f305730fb06eb2da6d17ce97a0479891
Dickson Afful <fiifi.dev@gmail.com>
2024-04-27 09:19:35 -0400

    fix step 5

---
+++ Assets/Scripts/HandleGameObjects/HandleNitrogenGunUtility.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+
+public class HandleNitrogenGunUtility : MonoBehaviour
+{
+    public UnityEvent OnActivate;
+    public UnityEvent OnDeactivate;
+    public UnityEvent OnObjectHit;
+
+    private bool HasObjectHit;
+    private bool HasAlreadyInvoked;
+    public string ObjectTag;
+
+    public GameObject NitrogenGunObject;
+
+
+
+    public ParticleSystem Particles;
+    private Animator MAnimator;
+    private XRGrabInteractable GrabInteractable;
+
+    public LayerMask GunLayerMask;
+    public Transform ShootSourse;
+    public float MaxRaycastDistance = 10;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (NitrogenGunObject != null)
+        {
+            MAnimator = NitrogenGunObject.GetComponent<Animator>();
+
+            GrabInteractable = NitrogenGunObject.GetComponent<XRGrabInteractable>();
+            GrabInteractable.activated.AddListener(StartGun);
+            GrabInteractable.deactivated.AddListener(StopGun);
+        }
+
+    }
+
+    private void OnDisable()
+    {
+        if (GrabInteractable != null)
+        {
+            GrabInteractable.activated.RemoveListener(StartGun);
+            GrabInteractable.deactivated.RemoveListener(StopGun);
+        }
+
+    }
+
+    private void StopGun(DeactivateEventArgs args)
+    {
+        MAnimator.SetTrigger("TrRelease");
+        Particles.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        OnDeactivate.Invoke();
+        HasObjectHit = false;
+        HasAlreadyInvoked = false;
+    }
+
+    private void StartGun(ActivateEventArgs args)
+    {
+        MAnimator.SetTrigger("TrPull");
+        Particles.Play();
+        OnActivate.Invoke();
+        HasObjectHit = true;
+
+    }
+
+    void Update()
+    {
+        RaycastCheck();
+    }
+
+    public void RaycastCheck()
+    {
+        RaycastHit hit;
+
+        var hasHit = Physics.Raycast(
+            ShootSourse.position,
+            ShootSourse.forward,
+            out hit,
+            MaxRaycastDistance,
+            GunLayerMask);
+
+        var obj = hit.transform?.gameObject;
+        if (obj == null) return;
+
+        if (obj.tag != ObjectTag) return;
+
+        var isHitValid = hasHit && HasObjectHit;
+
+        if (isHitValid && !HasAlreadyInvoked)
+        {
+            OnObjectHit.Invoke();
+            HasAlreadyInvoked = true;
+        }
+    }
+}
--- Assets/Scripts/Stages/StageOne/StepFive.cs
+++ Assets/Scripts/Stages/StageOne/StepFive.cs
@@ -3,73 +3,22 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class StepFive : MonoBehaviour, IProgressBar
+public class StepFive : MonoBehaviour
 {
     public StageScriptableObject StageScriptable;
-    private bool HasProgressBar { get; set; } = true;
-  
-    private bool IsStepFive { get; set; }
 
-    private void OnEnable()
+    public virtual void HandleStep()
     {
-       // ProgressBarUtility.OnComplete += HandleOnComplete;
-        HandleNitrogenGun.OnHitAcivate += HandleHitActivate;
-    }
-
-    private void OnDisable()
-    {
-       // ProgressBarUtility.OnComplete -= HandleOnComplete;
-        HandleNitrogenGun.OnHitAcivate -= HandleHitActivate;
-    }
-
-
-    void HandleHitActivate(GameObject obj, bool isHit, bool isHitPrev, RaycastStatusEnum raycastStatus)
-    {
-        if (raycastStatus == RaycastStatusEnum.Active && isHit && obj?.tag == "Wafer")
-        {
-            if (!IsStepFive) IsStepFive = true;
-
-            HandleOnComplete();
-        }
-        //else if (raycastStatus == RaycastStatusEnum.Inactive)
-        //{
-        //    if (!isHit && !isHitPrev) DisableProgress();
-        //}
-    }
-
-    public void HandleOnComplete()
-    {
-        // Runs if wafer is dropped in petri dish
         if (!CanPerformStep()) return;
-        
+
+        // Runs if wafer is dropped in petri 
         var step = StageScriptable.GetCurrentStep();
         step.IsDone = true;
-
-        //  finish stage
-        var stage = StageScriptable.GetCurrentStage();
-        stage.IsDone = true;
-        StageScriptable.SetNextStage();
     }
 
     private bool CanPerformStep()
     {
         var step = StageScriptable.GetCurrentStep();
-        return StageScriptable.ActiveStageId == 1 && IsStepFive && !step.IsDone && step.StepId == 5;
-    }
-
-
-    public void EnablepProgress()
-    {
-        if (!HasProgressBar) return;
-        ProgressBarUtility.Enable();
-        StartCoroutine(ProgressBarUtility.Tween());
-    }
-
-    public void DisableProgress()
-    {
-        if (!HasProgressBar) return;
-        ProgressBarUtility.Disable();
-        StopCoroutine(ProgressBarUtility.Tween());
-        IsStepFive = false;
+        return StageScriptable.ActiveStageId == 1 && !step.IsDone && step.StepId == 5;
     }
 }
